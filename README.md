af90880dd6e03c28598699391f72d6d4800e7aff
tazocar <tankodani@gmail.com>
2017-10-05 16:20:20 -0300

    Subiendo Cifrado Cesar

---
+++ README.md
@@ -0,0 +1,65 @@
+# Cifrado Cesar
+
+En este proyecto creamos un programa que cifra un mensaje por medio de la sustitución de caracteres.
+
+### Utilidad
++ Cifrar mensajes
++ Descifrar mensajes
+
+### Cifrado
++ Llamar a la función
+```
+javascript
+cipher()
+```
++ Pedir mensaje a cifrar
+	- **if** está vacío, pedir nuevo mensaje.
+	- **if** contiene números, pedir nuevo mensaje.
++ Pasar el mensaje a mayúsculas, *para comparar con códigos ASCII más adelante.*
++ Definir un arr vacío contenedor **textArr** *para contener números codificados*
++ Iterar en el mensaje.
+	- **if** el índice es " ", push [" "] a **textArr**
+	- Iterar entre los números 65 - 90 *(ASCII de las letras mayúscula)*
+	- **if** el número ASCII coincide con el código ASCII de la letra, pasa el número por la fórmula para codificarlo y **push** a **textArr**.
++ Crear Arr vacío **textConvert** *para contener letras*
++ iterar en **textArr**.
+	- **if** el índice es igual a [" "], **push** a **textConvert**.
+	- si no, convierte el número a letra y **push** a **textConvert**
++ Unir **textConvert** con un **join**.
+
+### Descifrado
++ Llamar a la función
+```
+javascript
+decipher()
+```
++ Pedir mensaje a descifrar
+	- **if** está vacío, pedir nuevo mensaje.
+	- **if** contiene números, pedir nuevo mensaje.
++ Pasar el mensaje a mayúsculas, *para comparar con códigos ASCII más adelante.*
++ Definir un arr vacío contenedor **textEncryptedArr** *para contener números decodificados*
++ Iterar en el mensaje.
+	- **if** el índice es " ", push [" "] a **textEncryptedArr**
+	- Iterar entre los números 65 - 90 *(ASCII de las letras mayúscula)*
+	- **if** el número ASCII coincide con el código ASCII de la letra, pasa el número por la fórmula para decodificarlo y **push** a **textEncryptedArr**.
++ Crear Arr vacío **textDecoded** *para contener letras*
++ iterar en **textEncryptedArr**.
+	- **if** el índice es igual a [" "], **push** a **textDecoded**.
+	- si no, convierte el número a letra y **push** a **textDecoded**.
++ Unir **textDecoded** con un **join**.
++ Pasar la primera letra a mayúsculas.
+
+
+
+#### Fórmulas
+
+```
+Cifrar
+(numLetraASCII - 65 + 33) % 26 + 65)
+
+Descifrar
+(numLetraASCII + 65 - 33) % 26 + 65)
+
+```
+
+##### Versión de archivo `0.01`
---
+++ app.js
@@ -0,0 +1,133 @@
+/*
+// indice + 33 módulo de 26 = posición Nueva letra
+(0+33)%26 //---> 7 (A = H)
+(4+33)%26 //---> 11 (E = L) 
+
+((índice - 33) módulo 26) + 26 = Posición antigua Letra
+(0-33)%26 //---> -0 (H = A)
+(11-33)%26 //--> -22 --> -22 + 26 = 4 (L = E)
+
+(79+65-33)%26+65 /// 72 --> inverso
+
+" " = 32
+
+*/
+
+//// CIFRAR ////
+
+//número del cifrado, es una variable por si se me ocurre pedirlo en un prompt
+function cipher(){
+	var num = 33;
+	do{
+		//pide el mensaje
+		var textInit = prompt("Ingresa Mensaje");
+		//itera en el mensaje
+		for (var i = 0; i < textInit.length; i++) {
+			//si un pompt al índice indicado es != a NaN, píde
+			//otro mensaje (no acepta numeros)
+			if(!isNaN(parseInt(textInit[i]))){
+				textInit = prompt("Mensaje sin numeros por favor");
+			}
+		}
+	} while ((!textInit))
+
+	//Paso el msje a mayúsculas
+	var textUpCase = textInit.toUpperCase();
+	var textArr = [];
+	//Itero en el msje en mayúsculas
+	for (var i = 0; i < textUpCase.length; i++) {
+		//si ese indice es un espacio vacío, mételo a textArr
+		if (textUpCase[i] === " ") {
+			textArr.push([" "])
+		}
+		//si no, itera entre números del 65 al 90
+		for (var j = 65; j <= 90; j++) {
+			//si el ASCII de la letra es igual al número J
+			if (textUpCase[i] == String.fromCharCode([j])) {
+				//Usa la fórmula del cifrado y ponlo en textArr
+				textArr.push(([j] -65 + num) % 26 + 65);
+			}
+		}
+	}
+	//variable contenedora de letras codificadas
+	var textConvert = [];
+	//itera en textArr
+	for (var k = 0; k < textArr.length; k++) {
+		//si el índice es = a espacio, mételo a textConvert
+		if (textArr[k] == " ") {
+			textConvert.push(textArr[k]);
+		//si no, cámbialo de letra a número.
+		}else {
+
+			textConvert.push(String.fromCharCode(textArr[k]))
+		}
+	}
+	var textJoin = textConvert.join("")
+
+	return textJoin;
+}
+
+/// CIFRAR ///
+
+
+
+//// DESCIFRAR ////
+// OVSH JVTV -- texto prueba
+
+//número del descifrado, es una variable por si se me ocurre pedirlo en un prompt
+
+function decipher(){
+	var num = 33;
+
+	do{
+		//pide el mensaje
+		var textEncrypted = prompt("Ingresa Mensaje Cifrado");
+		//itera en el mensaje
+		for (var i = 0; i < textEncrypted.length; i++) {
+			//si un pompt al índice indicado es != a NaN, píde
+			//otro mensaje (no acepta numeros)
+			if(!isNaN(parseInt(textEncrypted[i]))){
+				textEncrypted = prompt("Mensaje sin numeros por favor");
+			}
+		}
+	} while ((!textEncrypted))
+
+	//Paso el msje a mayúsculas
+	var textEncryptedUpCase = textEncrypted.toUpperCase();
+	var textEncryptedArr = [];
+	//Itero en el msje en mayúsculas
+	for (var i = 0; i < textEncryptedUpCase.length; i++) {
+		//si ese indice es un espacio vacío, mételo a textEncryptedArr
+		if (textEncryptedUpCase[i] === " ") {
+			textEncryptedArr.push([" "])
+		}
+		//si no, itera entre números del 65 al 90
+		for (var j = 65; j <= 90; j++) {
+			//si el ASCII de la letra es igual al número J
+			if (textEncryptedUpCase[i] == String.fromCharCode([j])) {
+				//Usa la fórmula de descifrado y ponlo en textEncryptedArr
+				textEncryptedArr.push(((j) + 65 - num) % 26 + 65);
+			}
+		}
+	}
+
+	//variable contenedora de letras codifivadas
+	var textDecoded = [];
+
+	//itera en textEncryptedArr
+	for (var k = 0; k < textEncryptedArr.length; k++) {
+		//si el índice es = a espacio, mételo a textDecoded
+		if (textEncryptedArr[k] == " ") {
+			textDecoded.push(textEncryptedArr[k]);
+		//si no, cambialo de número a letra
+		} else {
+			textDecoded.push(String.fromCharCode(textEncryptedArr[k]));
+		}
+	} 
+	var textDecodedJoin = textDecoded.join("")
+	var textDecodedToSend = textDecodedJoin.charAt(0)+(textDecodedJoin.slice(1,textDecodedJoin.length)).toLowerCase();
+
+	return textDecodedToSend;
+}
+
+/// DESCIFRAR ///
